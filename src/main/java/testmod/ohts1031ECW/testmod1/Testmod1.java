54b8798e0e25f8c477017d44ba494e6915edef4a
ohts1031ECW <ohashitamc_main@outlook.jp>
2025-01-03 21:08:55 +0900

    Add TestItem

--- src/main/java/testmod/ohts1031ECW/testmod1/Testmod1.java
+++ src/main/java/testmod/ohts1031ECW/testmod1/Testmod1.java
@@ -1,6 +1,7 @@
 package testmod.ohts1031ECW.testmod1;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+import testmod.ohts1031ECW.testmod1.item.ModItems;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Testmod1.MOD_ID)
@@ -24,6 +26,9 @@ public class Testmod1 {
 
     public Testmod1() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //register items
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -42,7 +47,9 @@ public class Testmod1 {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.TESTITEM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/testmod/ohts1031ECW/testmod1/item/ModItems.java
@@ -0,0 +1,24 @@
+package testmod.ohts1031ECW.testmod1.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import testmod.ohts1031ECW.testmod1.Testmod1;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Testmod1.MOD_ID);
+
+    //add item
+    public static final RegistryObject<Item> TESTITEM  = ITEMS.register(
+            "testitem",
+            () -> new Item(new Item.Properties())
+            );
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
